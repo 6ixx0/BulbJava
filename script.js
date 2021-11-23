00295f54d146287141a63d36efa3954a854b24c1
6ixx0 <iraph94@gmail.com>
2021-11-23 11:00:45 -0800

    bulb

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("btn1").onclick = function()
+{
+    document.getElementById("img1").src= "/img/pic_bulbon.gif"
+}
+
+document.getElementById("btn2").onclick = function()
+{
+    document.getElementById("img1").src = "/img/pic_bulboff.gif"
+}
